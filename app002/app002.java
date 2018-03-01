de045f5c1600cf11242aa9ca0fc1360fe0a29113
barnesscap <36700931+barnesscap@users.noreply.github.com>
2018-03-01 20:58:54 +0200

    Update app002.java

--- app002/app002.java
+++ app002/app002.java
@@ -5,10 +5,10 @@ import java.util.List;
 
 public class app002 {
     public static void main(String[] args) throws Exception {
-        Start();
+        start();
     }
 
-    private static void Start() throws Exception {
+    private static void start() throws Exception {
         int input=-1;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         while (input != 1 & input != 2) {
@@ -22,11 +22,11 @@ public class app002 {
                 System.out.println("Введите правильное значение!");
             }
         }
-        if (input==1) StartDeterminat();
-        if (input==2) StartTranform();
+        if (input==1) startDet();
+        if (input==2) startTransform();
     }
 
-    private static void StartTranform() throws Exception {
+    private static void startTransform() throws Exception {
         int height = -1;
         int width =-1;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
@@ -54,20 +54,9 @@ public class app002 {
                 width = -1;
             }
         }
-        int[][] array = new int[height][width];
-        for (int k = 0; k < height; k++) {
-            for (int n = 0; n < width; ) {
-                int r = k + 1;
-                int s = n + 1;
-                System.out.println("Введите число в ячейку ряд " + r + " столбец " + s + ":");
-                try {
-                    array[k][n] = Integer.parseInt(reader.readLine());
-                    n++;
-                } catch (IllegalArgumentException ex) {
-                    System.out.println("Веденное значение должно быть целочисельным числом! Попробуйте ещё раз.");
-                }
-            }
-        }
+
+        int[][] array = WorkWithArrays.fillMatrix(height,width);
+
         System.out.println();
         System.out.println();
         for (int k = 0; k < height; k++) {
@@ -78,7 +67,7 @@ public class app002 {
         }
         System.out.println();
         WorkWithArrays Work = new WorkWithArrays();
-        int [][] newArray = Work.TransformMatrice(array);
+        int [][] newArray = Work.transformMatrix(array);
 
         for (int k = 0; k < newArray.length; k++) {
             for (int n = 0; n < newArray[0].length; n++) {
@@ -89,7 +78,7 @@ public class app002 {
         System.out.println();
     }
 
-    private static void StartDeterminat() throws Exception {
+    private static void startDet() throws Exception {
         int sizeArray = -1;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         while (sizeArray <= 0) {
@@ -105,42 +94,39 @@ public class app002 {
             }
         }
         int[][] array = new int[sizeArray][sizeArray];
-        for (int k = 0; k < sizeArray; k++) {
-            for (int n = 0; n < sizeArray; ) {
-                int r = k + 1;
-                int s = n + 1;
-                System.out.println("Введите число в ячейку ряд " + r + " столбец " + s + ":");
-                try {
-                    array[k][n] = Integer.parseInt(reader.readLine());
-                    n++;
-                } catch (IllegalArgumentException ex) {
-                    System.out.println("Веденное значение должно быть целочисельным числом! Попробуйте ещё раз.");
+        try {
+            array = WorkWithArrays.fillMatrix(sizeArray,sizeArray);
+
+
+            System.out.println();
+            System.out.println();
+            for (int k = 0; k < sizeArray; k++) {
+                for (int n = 0; n < sizeArray; n++) {
+                    System.out.print(array[k][n] + "\t");
                 }
-            }
-        }
-        System.out.println();
-        System.out.println();
-        for (int k = 0; k < sizeArray; k++) {
-            for (int n = 0; n < sizeArray; n++) {
-                System.out.print(array[k][n] + "\t");
+                System.out.println();
             }
             System.out.println();
+            WorkWithArrays Work = new WorkWithArrays();
+            System.out.println(Work.detMatrix(array));
         }
-        System.out.println();
-        WorkWithArrays Work = new WorkWithArrays();
-        System.out.println(Work.DeterminatMatrix(array));
+        catch (Exception ex) {
+            System.err.println("Введен всего один элемент");
+            System.err.println();
+            System.err.println(array[0][0]);
+        }
     }
 }
 
 class WorkWithArrays {
 
-    private static int[][] ArraysRCRemove(int[][] array, int indX, int indY) { //Даємо масив і номери строки та стовбця
-        int [][] arrayRes = ArraysRowRemove(array, indX); //один метод для строк
-        arrayRes = ArraysColumnRemove(arrayRes, indY); //другий для стовбців
+    private static int[][] arraysRCRemove(int[][] array, int indX, int indY) { //Даємо масив і номери строки та стовбця
+        int [][] arrayRes = arraysRowRemove(array, indX); //один метод для строк
+        arrayRes = arraysColumnRemove(arrayRes, indY); //другий для стовбців
         return arrayRes;
     }
 
-    private static int[][] ArraysRowRemove(int[][] array, int indX) {
+    private static int[][] arraysRowRemove(int[][] array, int indX) {
         if (indX < array.length & indX > -1) {
             int newArray[][] = new int[array.length - 1][array[0].length];
             for (int k = 0; k < array.length; k++) {
@@ -156,7 +142,7 @@ class WorkWithArrays {
         } else return array;
     }
 
-    private static int[][] ArraysColumnRemove(int[][] array, int indY) {
+    private static int[][] arraysColumnRemove(int[][] array, int indY) {
         if (indY < array[0].length & indY > -1) {
             int newArray[][] = new int[array.length][array[0].length - 1];
             for (int a = 0; a < array.length; a++) {
@@ -172,14 +158,14 @@ class WorkWithArrays {
         } else return array;
     }
 
-    public static int DeterminatMatrix(int[][] matrix) {
+    public static int detMatrix(int[][] matrix) {
         int result = 0;
         List<Integer> list = new ArrayList<>();
         if (matrix.length == 2) {
             result = matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1];
         } else {
             for (int k =0; k<matrix[0].length; k++) {
-                list.add(matrix[0][k] * DeterminatMatrix(ArraysRCRemove(matrix, 0, k)));
+                list.add(matrix[0][k] * detMatrix(arraysRCRemove(matrix, 0, k)));
             }
         }
         for(int i = 0; i < list.size(); i++) {
@@ -189,7 +175,7 @@ class WorkWithArrays {
         return result;
     }
 
-    public static int [][] TransformMatrice (int [][] matrix) {
+    public static int [][] transformMatrix (int [][] matrix) {
         int [][]resMatrix = new int [matrix[0].length][matrix.length];
         for (int k=0; k<matrix.length; k++) {
             for (int n=0; n<matrix[0].length; n++) {
@@ -198,4 +184,29 @@ class WorkWithArrays {
         }
         return resMatrix;
     }
+
+    public static int [][] fillMatrix (int h, int w) throws Exception {
+        int[][] newArray = new int[h][w];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int k = 0; k < h; k++) {
+            for (int n = 0; n < w; ) {
+                int r = k + 1;
+                int s = n + 1;
+                System.out.println("Введите число в ячейку ряд " + r + " столбец " + s + ":");
+                try {
+                    newArray[k][n] = Integer.parseInt(reader.readLine());
+                    if (newArray.length == 1) {
+                        throw new Exception();
+                    } else {
+                        n++;
+                    }
+                } catch (IllegalArgumentException ex) {
+                    System.out.println("Веденное значение должно быть целочисельным числом! Попробуйте ещё раз.");
+                }
+
+            }
+        }
+        return newArray;
+    }
 }
+
